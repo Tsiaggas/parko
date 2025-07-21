a3514bc3f817f2688e1509cd48b11f1ea4e650d6
Sakis_Tsiaggas <ttsiaggas94@gmail.com>
2025-07-21 19:17:08 +0300

    dareios2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   trailingSlash: true,
   images: {
-    formats: ['image/webp', 'image/png', 'image/jpeg'],
+    formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 768, 1024, 1280, 1600],
     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
     minimumCacheTTL: 31536000, // 1 year caching
